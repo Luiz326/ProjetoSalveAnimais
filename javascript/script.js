76b9ee5db1c0110c70c00a9426ab3d51df068583
Luiz Mitsuru Dai <61781452+Luiz326@users.noreply.github.com>
2021-11-08 14:44:37 -0300

    javascript que atualiza o ano

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,12 @@
 let data = new Date();
-let ano = document.getElementById("ano").textContent ="Copyright@ " + data.getFullYear();
+let ano = document.getElementById("ano");
 
+function inicializa(){
+    setInterval(()=>{
+        ano.textContent ="Copyright@ " + data.getFullYear();
+    },1000)
+}
+inicializa();
 
     window.addEventListener("scroll", function(){
     let header = document.querySelector("header");
